f67a8e16245f25c9ae6a91207cefa07d7199e4a5
Paramita Basu <paramita.das@tcs.com>
2017-04-26 00:39:26 +0530

    first version

---
+++ run_analysis.R
@@ -0,0 +1,43 @@
+library(dplyr)
+
+#Read train data
+Y_train <- read.table("./UCI HAR Dataset/train/Y_train.txt")
+X_train <- read.table("./UCI HAR Dataset/train/X_train.txt")
+Sub_train <- read.table("./UCI HAR Dataset/train/subject_train.txt")
+
+#Read test data
+Y_test <- read.table("./UCI HAR Dataset/test/Y_test.txt")
+X_test <- read.table("./UCI HAR Dataset/test/X_test.txt")
+Sub_test <- read.table("./UCI HAR Dataset/test/subject_test.txt")
+
+#Read features
+features_names <- read.table("./UCI HAR Dataset/features.txt")
+
+#Read Activity labels
+activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt")
+
+# 1. Merges the training and the test sets to create one data set.
+X_MergeData <- rbind(X_train, X_test)
+Y_MergeData <- rbind(Y_train, Y_test)
+Subject_MergeData <- rbind(Sub_train, Sub_test)
+
+# 2. Extracts only the measurements on the mean and standard deviation for each measurement.
+selected_var <- variable_names[grep("mean\\(\\)|std\\(\\)",variable_names[,2]),]
+X_MergeData <- X_MergeData[,selected_var[,1]]
+
+# 3. Uses descriptive activity names to name the activities in the data set
+colnames(Y_MergeData) <- "activity"
+Y_MergeData$activitylabel <- factor(Y_MergeData$activity, labels = as.character(activity_labels[,2]))
+activitylabel <- Y_MergeData[,-1]
+
+# 4. Appropriately labels the data set with descriptive variable names.
+colnames(X_MergeData) <- variable_names[selected_var[,1],2]
+
+# 5. From the data set in step 4, creates a second, independent tidy data set with the average
+# of each variable for each activity and each subject.
+colnames(Sub_total) <- "subject"
+total <- cbind(X_MergeData, activitylabel, Subject_MergeData)
+total_mean <- total %>% group_by(activitylabel, subject) %>% summarize_each(funs(mean))
+write.table(total_mean, file = "./UCI HAR Dataset/tidydata.txt", row.names = FALSE, col.names = TRUE)
+
+
